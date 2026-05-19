ca7076ab4b6a755de6bdcdffe08d83d5c6103512
jpirnay <jens@pirnay.com>
2026-05-19 20:03:04 +0200

    Add ++ to logo

--- src/activities/boot_sleep/BootActivity.cpp
+++ src/activities/boot_sleep/BootActivity.cpp
@@ -6,6 +6,20 @@
 #include "fontIds.h"
 #include "images/Logo120.h"
 
+namespace {
+void drawPlusOutline(const GfxRenderer& renderer, int x, int y, int size, int armThickness) {
+  const int armOffset = (size - armThickness) / 2;
+  renderer.drawRect(x, y + armOffset, size, armThickness, true);
+  renderer.drawRect(x + armOffset, y, armThickness, size, true);
+  renderer.drawLine(x + armOffset + 1, y + armOffset, x + armOffset + armThickness - 2, y + armOffset, false);
+  renderer.drawLine(x + armOffset + 1, y + armOffset + armThickness - 1, x + armOffset + armThickness - 2,
+                    y + armOffset + armThickness - 1, false);
+  renderer.drawLine(x + armOffset, y + armOffset + 1, x + armOffset, y + armOffset + armThickness - 2, false);
+  renderer.drawLine(x + armOffset + armThickness - 1, y + armOffset + 1, x + armOffset + armThickness - 1,
+                    y + armOffset + armThickness - 2, false);
+}
+}  // namespace
+
 void BootActivity::onEnter() {
   Activity::onEnter();
   RenderLock lock(*this);
@@ -13,8 +27,22 @@ void BootActivity::onEnter() {
   const auto pageWidth = renderer.getScreenWidth();
   const auto pageHeight = renderer.getScreenHeight();
 
+  constexpr int LOGO_SIZE = 120;
+  constexpr int PLUS_SIZE = LOGO_SIZE / 3;
+  constexpr int PLUS_ARM_THICKNESS = PLUS_SIZE / 3;
+
+  const int logoX = (pageWidth - LOGO_SIZE) / 2;
+  const int logoY = (pageHeight - LOGO_SIZE) / 2;
+
   renderer.clearScreen();
-  renderer.drawImage(Logo120, (pageWidth - 120) / 2, (pageHeight - 120) / 2, 120, 120);
+  renderer.drawImage(Logo120, logoX, logoY, LOGO_SIZE, LOGO_SIZE);
+
+  const int plusY = logoY - PLUS_SIZE / 2;
+  const int plus1X = logoX + LOGO_SIZE;
+  const int plus2X = plus1X + PLUS_SIZE + 2;
+  drawPlusOutline(renderer, plus1X, plusY, PLUS_SIZE, PLUS_ARM_THICKNESS);
+  drawPlusOutline(renderer, plus2X, plusY, PLUS_SIZE, PLUS_ARM_THICKNESS);
+
   renderer.drawCenteredText(UI_10_FONT_ID, pageHeight / 2 + 70, tr(STR_CROSSPOINT), true, EpdFontFamily::BOLD);
   renderer.drawCenteredText(SMALL_FONT_ID, pageHeight / 2 + 95, tr(STR_BOOTING));
   renderer.drawCenteredText(SMALL_FONT_ID, pageHeight - 30, CROSSPOINT_VERSION);
